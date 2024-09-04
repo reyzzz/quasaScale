ffec9ebb9500d451683b20766b256e425f34f204
Ribal Azar <ribala94@gmail.com>
2024-09-04 13:25:38 +0300

    Finished main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header>
       <q-toolbar>
         <q-btn
           flat
@@ -9,20 +9,57 @@
           icon="menu"
           aria-label="Menu"
           @click="toggleLeftDrawer"
+          v-if="$q.platform.is.desktop"
         />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title> QuasaScale App </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      v-if="$q.platform.is.desktop"
+    >
       <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+        <q-item-label header class="text-primary text-h6 title">
+          HeadScale
+        </q-item-label>
+        <template v-for="(link, index) in linksList" :key="index">
+          <q-item clickable v-ripple :to="{ name: link.route }">
+            <q-item-section avatar>
+              <q-icon :name="link.icon" size="md" />
+            </q-item-section>
+            <q-item-section class="text-h6">{{ link.label }}</q-item-section>
+          </q-item>
+        </template>
       </q-list>
     </q-drawer>
-
+    <q-footer
+      class="bg-white footer-top-border q-py-sm iphone-bottom-notch"
+      v-if="$q.platform.is.mobile"
+    >
+      <q-tabs
+        no-caps
+        dense
+        indicator-color="transparent"
+        :active-color="$q.dark.isActive ? 'white' : 'black'"
+        :class="$q.dark.isActive ? 'text-dark-primary' : 'text-blue-grey-7'"
+        align="justify"
+      >
+        <q-route-tab
+          v-for="link in linksList"
+          :key="link.title"
+          :to="{ name: link.route }"
+          :name="link.title"
+          :icon="link.icon"
+          :label="link.label"
+          active-class="text-primary"
+          :ripple="false"
+        />
+      </q-tabs>
+    </q-footer>
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -31,13 +68,21 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-
+import { useQuasar } from 'quasar'
 defineOptions({
   name: 'MainLayout',
 })
 
+const $q = useQuasar()
 const leftDrawerOpen = ref(false)
-
+const linksList = ref([
+  {
+    title: 'User',
+    icon: 'person',
+    route: 'user',
+    label: 'User',
+  },
+])
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
